cb05af415b9341b48ef00c34949df39ce994a41d
HngChau <23130029@st.hcmuaf.edu.vn>
2025-12-24 19:19:58 +0700

    cấu hình file setting và file db

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -92,13 +92,22 @@ WSGI_APPLICATION = 'BookStore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'kMrHsprQMdDCkGlZNpsqLfjVtPQgImBR',
+        'HOST': 'hopper.proxy.rlwy.net',
+        'PORT': '55302',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
